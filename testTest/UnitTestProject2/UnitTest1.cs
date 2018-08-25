8a98b09c0df54e5a2327b83ca623a558784f254d
student <student@101-08>
2018-08-25 17:31:52 +0300

    Добавлены все тесты

---
+++ testTest/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using testTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Test t = new Test();
+        [TestMethod]
+        public void TestSum()
+        {
+            int a=3, b=2;
+            int exp = 5;
+            int tmp = t.Exmpl1(a, b);
+            Assert.AreEqual(tmp, exp);
+        }
+        [TestMethod]
+        public void TestSecret()
+        {
+            int a = 6;
+            bool b = t.Exmpl2(a);
+            Assert.AreEqual(b, true);
+        }
+        [TestMethod]
+        public void TestOther()
+        {
+            int a = 3, b = 2;
+            int exp = 6;
+            int tmp = t.Exmpl3(a, b);
+            Assert.AreEqual(tmp, exp);
+        }
+    }
+}
